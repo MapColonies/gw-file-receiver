a5ebb325e402c6b8e49e8be33110087eb42f7442
Shahar s <cl-shaharshavit-rnd@users.noreply.github.com>
2021-11-01 10:41:20 +0200

    chore: cleanup and cr

--- src/providers/storageProvider.ts
+++ src/providers/storageProvider.ts
@@ -1,6 +1,5 @@
 import { Readable } from 'stream';
 
 export interface IStorageProvider {
-  //createWriteStream: (path: string) => Promise<Writable>;
   saveFile: (path: string, contentStream: Readable) => Promise<unknown>;
 }
